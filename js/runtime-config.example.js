6e1431b72dc0824dc8bdc2407bd0d5c14ae86ca3
Ryan Duncuft <ryanduncuft23@gmail.com>
2026-02-05 22:57:42 +0000

    v0.1.1

---
+++ js/runtime-config.example.js
@@ -0,0 +1,17 @@
+window.RYZE_RUNTIME_CONFIG = {
+  firebase: {
+    apiKey: 'YOUR_FIREBASE_API_KEY',
+    authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN',
+    projectId: 'YOUR_FIREBASE_PROJECT_ID',
+    appId: 'YOUR_FIREBASE_APP_ID'
+  },
+  appCheck: {
+    siteKey: 'YOUR_RECAPTCHA_SITE_KEY',
+    debugToken: ''
+  },
+  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID',
+  paypalPlanIds: {
+    all: 'P-REPLACE-ALLACCESS'
+  },
+  paypalSinglePrice: '1.29'
+};
---
+++ js/runtime-config.js
@@ -0,0 +1,18 @@
+// Public client config only. Do not add private keys or service account secrets.
+window.RYZE_RUNTIME_CONFIG = {
+  firebase: {
+    apiKey: 'AIzaSyDZtjCx1MqBbZ8RqibJgGPDVG4WZP6D608',
+    authDomain: 'ryzeplus.firebaseapp.com',
+    projectId: 'ryzeplus',
+    appId: '1:480899269938:web:b9d5145d5288f03f69032b'
+  },
+  appCheck: {
+    siteKey: '6Ld7AWIsAAAAAIIwQDytJf_s89D_c03BKyVYJml8',
+    debugToken: ''
+  },
+  paypalClientId: 'YOUR_PAYPAL_CLIENT_ID',
+  paypalPlanIds: {
+    all: 'P-REPLACE-ALLACCESS'
+  },
+  paypalSinglePrice: '1.29'
+};
